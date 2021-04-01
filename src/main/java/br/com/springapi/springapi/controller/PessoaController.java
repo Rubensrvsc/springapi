4a0a6a82e313b5b1a1bd7b9bd50e273ff109bf54
rubensrvsc <Rubensspfc100@gmail.com>
2021-04-01 17:56:35 -0300

    feat: add post Pessoa

--- src/main/java/br/com/springapi/springapi/controller/PessoaController.java
+++ src/main/java/br/com/springapi/springapi/controller/PessoaController.java
@@ -1,8 +1,10 @@
 package br.com.springapi.springapi.controller;
 
 import java.util.List;
+import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,6 +21,11 @@ public class PessoaController {
     @RequestMapping(value="/pessoa", method = RequestMethod.GET)
     public List<Pessoa> Get(){
        return pessoaRepository.findAll(); 
+    }
+
+    @RequestMapping(value="/pessoa",method = RequestMethod.POST)
+    public Pessoa Post(@Valid @RequestBody Pessoa pessoa){
+        return pessoaRepository.save(pessoa);
     }
 
     
