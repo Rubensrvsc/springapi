a42ef1d6194b16c1ce72f97f7940725346dd3e4f
rubensrvsc <Rubensspfc100@gmail.com>
2021-04-01 18:02:05 -0300

    feat: GetById

--- src/main/java/br/com/springapi/springapi/controller/PessoaController.java
+++ src/main/java/br/com/springapi/springapi/controller/PessoaController.java
@@ -1,9 +1,15 @@
 package br.com.springapi.springapi.controller;
 
 import java.util.List;
+import java.util.Optional;
+
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -26,6 +32,15 @@ public class PessoaController {
     @RequestMapping(value="/pessoa",method = RequestMethod.POST)
     public Pessoa Post(@Valid @RequestBody Pessoa pessoa){
         return pessoaRepository.save(pessoa);
+    }
+
+    @RequestMapping(value="/pessoa/{id}", method = RequestMethod.GET)
+    public ResponseEntity<Pessoa> GetById(@PathVariable(value="id") long id){
+        Optional<Pessoa> pessoa = pessoaRepository.findById(id);
+        if(pessoa.isPresent()) 
+            return new ResponseEntity<Pessoa>(pessoa.get(),HttpStatus.OK);
+        else
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND)
     }
 
     
